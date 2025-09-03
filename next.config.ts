3c989bf4666dcdb1bc2a92f107b3feecf6f31887
devjsantos <jojo.santos.dev@gmail.com>
2025-09-03 21:00:34 +0800

    enhance image profile

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['img.clerk.com'], // allow Clerk's image CDN
+  },
 };
 
 export default nextConfig;
